8df57a7eb2b1a31e05d9310053548194791d0707
George T <n35.nnlpd@slmail.me>
2022-01-22 19:39:11 +0000

    FizzBuzz completed

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,4 +1,19 @@
 // TODO: Write your function in this file
+function fizzBuzz(lower, upper) {
+    const numbers = [];
+
+    for (let i = lower; i <= upper; i++) {
+        if ((i % 3 === 0) && (i % 5 === 0))
+        numbers.push('FizzBuzz')
+        else if (i % 3 === 0)
+        numbers.push('Fizz')
+        else if (i % 5 === 0)
+        numbers.push('Buzz')
+        else
+        numbers.push(i)
+    }
+     return numbers;   
+}
 
 // TODO: Change undefined below to the name of your function
-module.exports = undefined
+module.exports = fizzBuzz
